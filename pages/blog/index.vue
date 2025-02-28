e726ff29e945095f36e587ea89045f7108f32498
bamosguinley <bamosgpro@gmail.com>
2025-02-28 11:29:07 +0100

    install nuxtui pro

---
+++ pages/blog/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <h1>Blog</h1>
+        <p>Here is a list of blog posts</p>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <h1>bmsgdev</h1>
+        <p>bmsgdev</p>
+    </div>
+</template>
+
+<style scoped></style>
