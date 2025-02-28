b835c63a9a56a398ea81af01aaec5dbf5bc71558
bamosguinley <bamosgpro@gmail.com>
2025-02-28 18:04:42 +0100

    add toc

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
 const route= useRoute();
 const { data: post } = await useAsyncData("post", () => queryCollection("blog").path(route.path).first());
+const { data: surround } = await useAsyncData(`surround-${route.path}` , () => {
+    return queryCollectionItemSurroundings("blog",route?.path,{
+       fields: ['description'],
+    })
+});
 </script>
 <template>
-    <UBlogPost
-    v-if="post"
-        :title="post.title"
-        :description="post.description"
-        :authors="post.auteurs"
-        :image="post.image"
-        :date="formatDate(post.date)"
-        :post="post"
-        :badge="{...post.badge,variant:'outline'}"
-        :to="post.path"
-      >
-      </UBlogPost>
+    <UContainer v-if="post">
+        <UPageHeader :title="post.title" :description="post.description">
+            <template #headline>
+               <UBadge v-bind="post.badge" variant="subtle"/>
+               <time :datetime="post.date">{{formatDate(post.date)}}</time> 
+            </template>
+            <UButton v-for="(auteur,index) in post.auteurs" :key="index" color="white" variant='ghost' target="_blank" :to="auteur.to">
+                <UAvatar v-bind="auteur.avatar" size="sm" class="mr-2"/>
+                {{auteur.name}}
+            </UButton>
+        </UPageHeader>
+        <UPage>
+            <UPageBody prose>
+                <ContentRenderer :value="post"/>
+                <!-- Séparateur -->
+                <UDivider v-if="surround" class="my-4"/>
+                <UContentSurround class="mt-8" v-if="surround" :surround="surround"/>
+            </UPageBody>
+            <template v-if="post.body.toc?.links" #rigth>
+                <UContentToc :links="post.body.toc.links">
+                </UContentToc>
+            </template>
+        </UPage>
+    </UContainer>
 </template>
 
 <style scoped></style>
