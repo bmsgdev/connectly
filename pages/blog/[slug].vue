dbdbc1ddae9870d8fed19691b9fc16b518fc312a
bamosguinley <bamosgpro@gmail.com>
2025-02-28 18:06:26 +0100

    update toc

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,37 +1,45 @@
 <script setup lang="ts">
-const route= useRoute();
-const { data: post } = await useAsyncData("post", () => queryCollection("blog").path(route.path).first());
-const { data: surround } = await useAsyncData(`surround-${route.path}` , () => {
-    return queryCollectionItemSurroundings("blog",route?.path,{
-       fields: ['description'],
-    })
+const route = useRoute();
+const { data: post } = await useAsyncData("post", () =>
+  queryCollection("blog").path(route.path).first()
+);
+const { data: surround } = await useAsyncData(`surround-${route.path}`, () => {
+  return queryCollectionItemSurroundings("blog", route?.path, {
+    fields: ["description"],
+  });
 });
 </script>
 <template>
-    <UContainer v-if="post">
-        <UPageHeader :title="post.title" :description="post.description">
-            <template #headline>
-               <UBadge v-bind="post.badge" variant="subtle"/>
-               <time :datetime="post.date">{{formatDate(post.date)}}</time> 
-            </template>
-            <UButton v-for="(auteur,index) in post.auteurs" :key="index" color="white" variant='ghost' target="_blank" :to="auteur.to">
-                <UAvatar v-bind="auteur.avatar" size="sm" class="mr-2"/>
-                {{auteur.name}}
-            </UButton>
-        </UPageHeader>
-        <UPage>
-            <UPageBody prose>
-                <ContentRenderer :value="post"/>
-                <!-- Séparateur -->
-                <UDivider v-if="surround" class="my-4"/>
-                <UContentSurround class="mt-8" v-if="surround" :surround="surround"/>
-            </UPageBody>
-            <template v-if="post.body.toc?.links" #rigth>
-                <UContentToc :links="post.body.toc.links">
-                </UContentToc>
-            </template>
-        </UPage>
-    </UContainer>
+  <UContainer v-if="post">
+    <UPageHeader :title="post.title" :description="post.description">
+      <template #headline>
+        <UBadge v-bind="post.badge" variant="subtle" />
+        <time :datetime="post.date">{{ formatDate(post.date) }}</time>
+      </template>
+      <UButton
+        v-for="(auteur, index) in post.auteurs"
+        :key="index"
+        color="white"
+        variant="ghost"
+        target="_blank"
+        :to="auteur.to"
+      >
+        <UAvatar v-bind="auteur.avatar" size="sm" class="mr-2" />
+        {{ auteur.name }}
+      </UButton>
+    </UPageHeader>
+    <UPage>
+      <UPageBody prose>
+        <ContentRenderer :value="post" />
+        <!-- Séparateur -->
+        <UDivider v-if="surround" class="my-4" />
+        <UContentSurround class="mt-8" v-if="surround" :surround="surround" />
+      </UPageBody>
+      <template v-if="post.body?.toc?.links?.length" #right>
+        <UContentToc :links="post.body.toc.links" />
+      </template>
+    </UPage>
+  </UContainer>
 </template>
 
 <style scoped></style>
